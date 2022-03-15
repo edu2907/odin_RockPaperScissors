4989c79d224754a4e2ba5e5f27f6e33b6ffca6c9
Eduardo <eduardo.carneiro2907@gmail.com>
2022-03-14 21:24:20 -0300

    Get one of the choices in the page
    This commit edited script.js to start interacting with the page using
    DOM. By now, the website will get one of the three choices and play a
    round, but no output by now.

--- script.js
+++ script.js
@@ -1,96 +1,58 @@
-const possibilities = ['rock', 'paper', 'scissors']
-// Randomly chooses one element from array for computer
-function computerPlay() {
-    let i = Math.floor(Math.random() * (2 - 0 + 1))
-    return possibilities[i]
-}
-// Main function
-function game() {
-    //Where player will type your choice
-    function playerInput() {
-        playerSelection = prompt(`Round ${i + 1}\nRock Paper Scissors!`)
-        if (playerSelection == null) {
-            alert("You cannot cancel!")
-            return playerInput()
-        }
-        else {
-            playerSelection = playerSelection.toLowerCase()
-            if (playerSelection === possibilities[0] || playerSelection === possibilities[1] || playerSelection === possibilities[2]) return playerSelection
-
-            else {
-                alert('You typed wrong!\n You can only choose "rock", "paper" or "scissors". Try again!')
-                return playerInput()
-            }
-        }
+let options = ['rock', 'paper', 'scissors']
+options.forEach(option => document.getElementById(option).addEventListener('click', game))
+function game(element) {
+    // Randomly chooses one element from array for computer
+    function computerPlay() {
+        let i = Math.floor(Math.random() * (2 - 0 + 1))
+        return options[i]
     }
-    //Checks who's the winner of the round
-    function round(playerSelection, computerSelection) {
 
+    //Checks who's the winner of the round
+    function round() {
         if (playerSelection == 'rock') {
             if (computerSelection == 'scissors') {
-                alert("Rock wins Scissors!")
                 return true
             }
             else if (computerSelection == 'paper') {
-                alert("Rock loses to Paper!")
                 return false
             }
             else if (computerSelection == 'rock') {
-                alert("Tie!")
                 return null
             }
         }
         else if (playerSelection == 'paper') {
             if (computerSelection == 'rock') {
-                alert("Paper wins Rock!")
                 return true
             }
             else if (computerSelection == 'scissors') {
-                alert("Papers loses to Scissors!")
                 return false
             }
             else if (computerSelection == 'paper') {
-                alert("Tie!")
                 return null
             }
-        }
-        else if (playerSelection == 'scissors') {
+        } else if (playerSelection == 'scissors') {
             if (computerSelection == 'paper') {
-                alert("Scissors wins Paper!")
                 return true
             }
             else if (computerSelection == 'rock') {
-                alert("Scissors loses to Rock!")
                 return false
             }
             else if (computerSelection == 'scissors') {
-                alert("Tie!")
                 return null
             }
         }
     }
-    //Name says all about 
-    function scoreChecker(score1, score2) {
-        if (score1 > score2) return "Player"
-        else if (score2 > score1) return "Computer"
-        else return "Tie"
-    }
-    // Where game() function actually starts
-    let playerSelection;
-    let playerScore = 0
-    let computerScore = 0
-    for (i = 0; i < 5; i++) {
-        playerSelection = playerInput()
-        let roundResult = round(playerSelection, computerPlay())
-        if (roundResult == true) playerScore++
-        else if (roundResult == false) computerScore++
-        else continue
-    }
-    return score = {
-        Player: playerScore,
-        Computer: computerScore,
-        Winner: scoreChecker(playerScore, computerScore)
+    let playerSelection = element.target.id;
+    let computerSelection = computerPlay()
+    switch(round()) {
+        case true: 
+        
+        break;
+        case false: 
+
+        break;
+        case null: 
+
+         break;
     }
 }
-
-console.table(game())
