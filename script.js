2164091a7f7e6451f16639afef3abcb989d4eeb1
Eduardo <eduardo.carneiro2907@gmail.com>
2022-03-16 20:42:26 -0300

    Display round results on the page
    Basically the output of the code, so the game at this commit is playable,
    but not finished yet.

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-let options = ['rock', 'paper', 'scissors']
-options.forEach(option => document.getElementById(option).addEventListener('click', game))
-function game(element) {
+let playerElements = {
+    boxes: document.querySelectorAll(".box.human"),
+    choice: document.querySelector("#humanChoice") ,
+    score : document.querySelector(".human.scorepoints")
+};
+let computerElements = {
+    boxes: document.querySelectorAll(".box.computer"),
+    choice: document.querySelector("#computerChoice"),
+    score: document.querySelector(".computer.scorepoints")
+};
+let middleText = document.getElementById('middleText')
+let playerScore = 0;
+let computerScore = 0;
+let options = ['rock', 'paper', 'scissors'];
+options.forEach(option => document.getElementById(option).addEventListener('click', playRound));
+
+function playRound(element) {
     // Randomly chooses one element from array for computer
     function computerPlay() {
         let i = Math.floor(Math.random() * (2 - 0 + 1))
@@ -8,7 +22,7 @@ function game(element) {
     }
 
     //Checks who's the winner of the round
-    function round() {
+    function roundWinner() {
         if (playerSelection == 'rock') {
             if (computerSelection == 'scissors') {
                 return true
@@ -42,17 +56,32 @@ function game(element) {
             }
         }
     }
+
     let playerSelection = element.target.id;
-    let computerSelection = computerPlay()
-    switch(round()) {
+    let computerSelection = computerPlay();
+    playerElements.choice.src = `imgs/rps/${playerSelection}.png`;
+    computerElements.choice.src = `imgs/rps/${computerSelection}.png`;
+    switch(roundWinner()) {
         case true: 
-        
+        playerScore++
+        middleText.textContent = `${playerSelection} wins ${computerSelection}!`;
+        playerElements.boxes.forEach(box => box.style.backgroundColor = "#689612");
+        playerElements.score.textContent = playerScore;
+        computerElements.boxes.forEach(box => box.style.backgroundColor = "#962812");
         break;
+
         case false: 
-
+        computerScore++
+        middleText.textContent = `${playerSelection} loses to ${computerSelection}!`;
+        computerElements.boxes.forEach(box => box.style.backgroundColor = "#689612");
+        computerElements.score.textContent = computerScore;
+        playerElements.boxes.forEach(box => box.style.backgroundColor = "#962812");
         break;
-        case null: 
 
+        case null: 
+        middleText.textContent = "Tie!";
+        playerElements.boxes.forEach(box => box.style.backgroundColor = "#689612");
+        computerElements.boxes.forEach(box => box.style.backgroundColor = "#689612");
          break;
-    }
-}
+    };
+};
